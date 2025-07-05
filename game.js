dce46c1d7523cc0d9ea51b106df74b8ac80f20b7
Chonky Peanut <89221482+ChonkyPeanut@users.noreply.github.com>
2025-07-05 13:36:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -8,7 +8,8 @@ let player = {
   width: 50,
   height: 50,
   speed: 4,
-  color: "lime"
+  color: "lime",
+  gravity: -1
 };
 
 // WASD key state
@@ -39,6 +40,9 @@ function update() {
   if (player.y < 0) player.y = 0;
   if (player.x + player.width > canvas.width) player.x = canvas.width - player.width;
   if (player.y + player.height > canvas.height) player.y = canvas.height - player.height;
+
+  if (player.gravity != 0) = true;
+    player.y += player.speed += player.gravity;
 }
 
 // Draw player
